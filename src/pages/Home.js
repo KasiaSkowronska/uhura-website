c7511d609d701a6e86a3c774a09ba70704d769d2
kaglowka <krz.glowka@gmail.com>
2018-06-17 16:43:27 +0200

    Logo footer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,44 @@
-import { hot } from 'react-hot-loader';
+import {hot} from 'react-hot-loader';
 import * as React from 'react';
 
 import '../styles/theme.sass';
 import './Home.sass';
 
-
+import logoUW from '../assets/uw-pl.png';
+import logoIUW from '../assets/iuw-pl.png';
+import logoWUM from '../assets/wum-pl.png';
 
 
 class Home extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  
+  static logos = [
+    {
+      class: 'logo-uw',
+      src: logoUW,
+    },
+    {
+      class: 'logo-iuw',
+      src: logoIUW,
+    },
+    {
+      class: 'logo-WUM',
+      src: logoWUM,
+    },
+  ];
+  
+  renderLogos() {
+    return (
+      <div className="logos">
+        {Home.logos.map(logo =>
+          (<div className="logo-item">
+              <img src={logo.src} className={logo.class}/>
+            </div>
+          )
+        )}
+      </div>
+    );
+  }
+  
   render() {
     return (
       <div className="page">
@@ -24,10 +55,13 @@ class Home extends React.Component { // eslint-disable-line react/prefer-statele
             <h2>About Uhura</h2>
             <div>
               <p>
-                Every year thousands of people around the world lose their voice due to a cancer of upper air passages. Many of them lose their ability to speak.
+                Every year thousands of people around the world lose their voice due to a cancer of upper air passages.
+                Many of them lose their ability to speak.
               </p>
               <p>
-                With Project Uhura we want to restore their freedom, by creating glasses for voice. With the help of technology the glasses will be able to understand and convey what Patient wants to say just from the movements of their muscles. We create the profession of tomorrow – the voice optician.
+                With Project Uhura we want to restore their freedom, by creating glasses for voice. With the help of
+                technology the glasses will be able to understand and convey what Patient wants to say just from the
+                movements of their muscles. We create the profession of tomorrow – the voice optician.
               </p>
             </div>
           </section>
@@ -35,16 +69,24 @@ class Home extends React.Component { // eslint-disable-line react/prefer-statele
             <h2>Our solution</h2>
             <div>
               <p>
-                Uhura physical interface process signal from various sources e.g. virtual keyboard, which could be used on any surface. It integrates speaker and EMG sensors in one device.
+                Uhura physical interface process signal from various sources e.g. virtual keyboard, which could be used
+                on any surface. It integrates speaker and EMG sensors in one device.
               </p>
               <p>
-                Their capabilities are coordinated by our program, UhuraOs, that reads the movements of face muscles, and then processess them into intonation, which is then applied to the words and played by the speaker. What is more, we include the voice banking technology, meaning that Uhura speaks with the Patient’s voice from before the surgery.
+                Their capabilities are coordinated by our program, UhuraOs, that reads the movements of face muscles,
+                and then processess them into intonation, which is then applied to the words and played by the speaker.
+                What is more, we include the voice banking technology, meaning that Uhura speaks with the Patient’s
+                voice from before the surgery.
               </p>
             </div>
           </section>
         </main>
+        <footer>
+          {this.renderLogos()}
+        </footer>
       </div>
     );
   }
 }
+
 export default hot(module)(Home);
